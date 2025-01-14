6cd8e27b6a87b5cbb108ca8823443344dc9fe655
Ruben2006676 <rubenlagolopez2006@gmail.com>
2025-01-14 14:51:50 +0100

    serverP1.py

--- serverP1.py
+++ serverP1.py
@@ -9,7 +9,6 @@ class User:
     def __str__(self):
         return "User:" + self.username + " pass:" + self.password + " email:" + self.email
 
-
 ListUsers = [
     User(id=1, username="usuari1", password="12345", email="prova@gmail.com"),
     User(id=2, username="usuari2", password="123", email="usuari2@gmail.com"),
@@ -18,3 +17,23 @@ ListUsers = [
 
 for u in ListUsers:
     print(u)
+
+class DAOUsers:
+    def __init__(self):
+        self.users=ListUsers
+    
+    def getUserbyUsername(self,username):
+        for u in self.users:
+            if u.username==username:
+                return u
+        return None
+        
+daoUser = DAOUsers()
+
+print(daoUser.getUserbyUsername("usuari1"))
+u=daoUser.getUserbyUsername("usuari1")
+
+if(u):
+    print(u)
+else:
+    print("No trobat")
