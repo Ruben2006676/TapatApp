8461ada67a27df29c1d81e51e007282d77c689c5
Ruben2006676 <rubenlagolopez2006@gmail.com>
2025-01-14 14:26:57 +0100

    Add files via upload

---
+++ serverP1.py
@@ -0,0 +1,20 @@
+# Clase User
+class User:
+    def __init__(self, id, username, password, email):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.email = email
+    
+    def __str__(self):
+        return "User:" + self.username + " pass:" + self.password + " email:" + self.email
+
+
+ListUsers = [
+    User(id=1, username="usuari1", password="12345", email="prova@gmail.com"),
+    User(id=2, username="usuari2", password="123", email="usuari2@gmail.com"),
+    User(id=3, username="admin", password="12", email= "admin@proven.cat")
+    ]
+
+for u in ListUsers:
+    print(u)
