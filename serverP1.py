18a3fc93791b85d807f5f197c3f0d8121e3c1a34
Ruben2006676 <rubenlagolopez2006@gmail.com>
2025-01-14 15:01:12 +0100

    Update serverP1.py

--- serverP1.py
+++ serverP1.py
@@ -37,3 +37,13 @@ if(u):
     print(u)
 else:
     print("No trobat")
+
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="10050")
